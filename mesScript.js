b6a0be525ca13d5c5ed2cfade76097e4f69c6216
aminekhouaja <makredface@gmail.com>
2025-05-17 15:41:33 +0200

    update for sti cards

--- mesScript.js
+++ mesScript.js
@@ -12,6 +12,11 @@ function verif() {
     openModal("algok");
   }
 }
+function verif2() {
+  const ind = document.getElementById("methode_aff").selectedIndex;
+  sel = document.getElementById("methode_aff").options[ind].value;
+  openModal(sel);
+}
 function changeX() {
   const sel = document.getElementById("methode_rec").selectedIndex;
   if (sel == 0) {
@@ -31,6 +36,68 @@ function changeX() {
     document.getElementById("nomg").innerHTML = "droite";
   }
 }
+function changecard() {
+  // Créer une nouvelle carte
+  const card = document.getElementById("grrrr");
+  const sel = document.getElementById("methode_aff").selectedIndex;
+  if (sel == 1) {
+    card.innerHTML = `
+        <pre>
+$req= "SELECT Section, count(*) as 'Nbre d élèves'
+FROM elevee
+Group BY Section";
+$res = mysqli_query($cx, $req);
+echo "<i class="nomf">&lt;ul&gt;</i>";
+while( $T =<i class="nomf"> mysqli_fetch_array($res)</i>){
+  echo <i class="nomg">
+    "&lt;li&gt;".$T["Section"]." ".$T[1]."&lt;/li&gt"</i>;
+}
+echo"<i class="nomf">&lt;/ul&gt;</i>";
+        </pre>
+
+    `;
+  } else if (sel == 2) {
+    card.innerHTML = `
+        <pre>
+$req= "SELECT Section, count(*) as 'Nbre d élèves'
+FROM elevee
+Group BY Section";
+$res = mysqli_query($cx, $req);
+echo "<i class="nomf">&lt;select&gt;</i>";
+while( $T =<i class="nomf"> mysqli_fetch_array($res)</i>){
+  echo <i class="nomg">
+    "&lt;option value='".$T[1]."'&gt;".$T["Section"]."&lt;/option&gt"</i>;
+}
+echo"<i class="nomf">&lt;/select&gt;</i>";
+        </pre>
+
+    `;
+  } else {
+    card.innerHTML = `
+            <pre>
+$req= "SELECT Section, count(*) as 'Nbre d élèves'
+FROM elevee
+Group BY Section";
+$res = mysqli_query($cx, $req);
+echo "<i class="nomf">&lt;table border='2'&gt;</i><i class="nomg">
+&lt;tr&gt;
+&lt;th&gt;Section&lt;/th&gt;
+&lt;th&gt;Nombre d'élèves Inscrit&lt;/th&gt;
+&lt;/tr&gt;</i>";
+while( $T =<i class="nomf"> mysqli_fetch_array($res)</i>){
+  echo "<i class="nomg">
+  &lt;tr&gt;
+    &lt;td&gt;".$T["Section"]."&lt;/td&gt;
+    &lt;td&gt;".$T[1]."&lt;/td&gt;
+  &lt;/tr&gt;"</i>;
+}
+echo"<i class="nomf">&lt;/table&gt</i>";
+        </pre>`;
+  }
+
+  // Contenu de la carte
+}
+
 function addNewCard() {
   // Créer une nouvelle carte
   const card = document.getElementById("joe1");
